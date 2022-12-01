33387a5d6242a8b5dde24c690d20719fd94b1d7c
allesio pellicciotta <allesio.pellicciotta@appwise.be>
2022-12-01 08:41:11 +0100

    Solved Day01 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,19 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int = input.joinToString(separator = "\n")
+        .split("\n\n")
+        .maxOf { chunk ->
+            chunk.split("\n")
+                .map(String::toLong)
+                .sum()
+                .toInt()
+        }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
