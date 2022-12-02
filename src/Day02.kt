1bc6cf350414228f3400feb39f63a34490ccd243
allesio pellicciotta <allesio.pellicciotta@appwise.be>
2022-12-02 09:04:43 +0100

    Solved part 1

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,9 +1,34 @@
 fun main() {
-    fun part1(input: List<String>): Int = input.size
+    fun part1(input: List<String>) = input
+        .map { line ->
+            line.split(" ")
+                .map { it.first().toHand() }
+        }.sumOf { (opponent, player) ->
+            player.score.toLong() + playerScore(player, opponent)
+        }
 
     val testInput = readInput("Day02_test")
-    check(part1(testInput) == 15)
+    check(part1(testInput) == 15L)
 
     val input = readInput("Day02")
     println(part1(input))
+}
+
+private fun playerScore(player: Hand, opponent: Hand): Int = when {
+    player.score == opponent.score -> 3
+    (player.score % 3) + 1 == opponent.score -> 0
+    else -> 6
+}
+
+private fun Char.toHand() = when (this) {
+    'A', 'X' -> Hand.Rock
+    'B', 'Y' -> Hand.Paper
+    'C', 'Z' -> Hand.Scissors
+    else -> error("unknown hand: $this")
+}
+
+private enum class Hand(val score: Int) {
+    Rock(1),
+    Paper(2),
+    Scissors(3);
 }
