b7b599a0348fba1f1c5001ad3074fef17cb19857
allesio pellicciotta <allesio.pellicciotta@appwise.be>
2022-12-03 11:20:36 +0100

    Solved Day 3 part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,3 +1,5 @@
+import kotlin.system.measureNanoTime
+
 fun main() {
     fun part1(input: List<String>): Int = input.sumOf { line ->
         val compartmentSize = line.length / 2
@@ -8,14 +10,30 @@ fun main() {
             .priority
     }
 
+    fun part2(input: List<String>): Int = input.chunked(3).sumOf { group ->
+        group.first()
+            .first { c -> c in group[1] && c in group.last() }
+            .priority
+    }
+
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
 
     val input = readInput("Day03")
-    part1(input).also {
-        check(it == 8240)
-        println(it)
-    }
+    measureNanoTime {
+        part1(input).also {
+            check(it == 8240)
+            print(it)
+        }
+    }.let { println(" in ${it}ns") }
+
+    measureNanoTime {
+        part2(input).also {
+            check(it == 2587)
+            print(it)
+        }
+    }.let { println(" in ${it}ns") }
 }
 
 private val Char.priority: Int get() = code - if (isUpperCase()) 38 else 96
