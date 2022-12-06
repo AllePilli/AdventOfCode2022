6e5b943910634cb45a2f114c851919017e4fd7fb
allesio pellicciotta <allesio.pellicciotta@appwise.be>
2022-12-06 09:10:29 +0100

    Day 6 code cleanup

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,11 +1,11 @@
 fun main() {
-    fun part1(input: String): Int = 4 + input.windowed(4).indexOfFirst { markerCandidate ->
-        markerCandidate.toSet().size == markerCandidate.length
+    fun String.indexOfFirstDistinctSubString(length: Int) = length + windowed(length).indexOfFirst { candidate ->
+        candidate.toSet().size == candidate.length
     }
 
-    fun part2(input: String): Int = 14 + input.windowed(14).indexOfFirst { msgCandidate ->
-        msgCandidate.toSet().size == msgCandidate.length
-    }
+    fun part1(input: String): Int = input.indexOfFirstDistinctSubString(4)
+
+    fun part2(input: String): Int = input.indexOfFirstDistinctSubString(14)
 
     val testInput = readInput("Day06_test")
     testInput.zip(listOf(7, 5, 6, 10, 11)).forEach { (input, output) ->
@@ -20,6 +20,6 @@ fun main() {
         checkAndPrint(part1(input), 1702)
     }
     measureAndPrintTimeMillis {
-        checkAndPrint(part2(input))
+        checkAndPrint(part2(input), 3559)
     }
 }
