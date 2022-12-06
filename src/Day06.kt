1684eb8929c4a1455b3246cac8cf6d6930d1264d
allesio pellicciotta <allesio.pellicciotta@appwise.be>
2022-12-06 09:08:14 +0100

    Solved Day 6 part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -3,13 +3,23 @@ fun main() {
         markerCandidate.toSet().size == markerCandidate.length
     }
 
+    fun part2(input: String): Int = 14 + input.windowed(14).indexOfFirst { msgCandidate ->
+        msgCandidate.toSet().size == msgCandidate.length
+    }
+
     val testInput = readInput("Day06_test")
     testInput.zip(listOf(7, 5, 6, 10, 11)).forEach { (input, output) ->
         check(part1(input) == output)
     }
+    testInput.zip(listOf(19, 23, 23, 29, 26)).forEach { (input, output) ->
+        check(part2(input) == output)
+    }
 
     val input = readInput("Day06").first()
     measureAndPrintTimeMillis {
-        checkAndPrint(part1(input))
+        checkAndPrint(part1(input), 1702)
+    }
+    measureAndPrintTimeMillis {
+        checkAndPrint(part2(input))
     }
 }
