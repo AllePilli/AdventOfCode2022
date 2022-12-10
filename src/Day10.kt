b602379e6de015b4af473e7573bd6a364b8b39fe
allesio pellicciotta <allesio.pellicciotta@appwise.be>
2022-12-10 16:08:24 +0100

    Solved Day 10 part 2

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,3 +1,5 @@
+import kotlin.math.abs
+
 fun main() {
     fun List<String>.prepareInput(): List<Operation> {
         val addxRgx = """addx (-?\d+)""".toRegex()
@@ -30,12 +32,35 @@ fun main() {
         }.sum()
     }
 
+    fun part2(input: List<Operation>) {
+        var x = 1
+        var crtX = 0
+        var cycle = 0
+
+        input.forEach { op ->
+            repeat(op.completionTime) {
+                cycle++
+                print(if (abs(x - crtX) > 1) "." else "#")
+                crtX = cycle % 40
+                if (crtX == 0) println()
+            }
+
+            if (op is Operation.Addx) x += op.amt
+        }
+    }
+
     val testInput = readInput("Day10_test").prepareInput()
     check(part1(testInput) == 13140)
+    part2(testInput)
+    println()
 
     val input = readInput("Day10").prepareInput()
     measureAndPrintTimeMillis {
         checkAndPrint(part1(input), 13720)
+    }
+
+    measureAndPrintTimeMillis {
+        part2(input) // should be FBURHZCH
     }
 }
 
