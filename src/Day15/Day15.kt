d53a72f8be017e7ab7958613004d046a5ed35914
allesio pellicciotta <allesio.pellicciotta@appwise.be>
2023-02-04 16:45:56 +0100

    Code cleanup

--- src/Day15/Day15.kt
+++ src/Day15/Day15.kt
@@ -10,7 +10,6 @@ import kotlin.math.min
 fun main() {
     val lineRgx = """Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)""".toRegex()
 
-    // Return a list of sensors and a list of beacons
     fun List<String>.prepareInput() = map { line ->
         val coordinates = lineRgx.matchEntire(line)?.groupValues
             ?.drop(1)
@@ -18,39 +17,43 @@ fun main() {
             ?: throw IllegalArgumentException("line: $line")
 
         Pair(
-            Pair(coordinates[0], coordinates[1]),
-            Pair(coordinates[2], coordinates[3]),
+            Sensor(coordinates[0] to coordinates[1]),
+            Beacon(coordinates[2] to coordinates[3])
         )
-    }.let {
-        it.map { pair -> pair.first } to
-                it.map { pair -> pair.second }.distinct()
     }
 
-    fun part1(row: Int, sensors: List<Pair<Int, Int>>, beacons: List<Pair<Int, Int>>): Int {
-        val minX = min(sensors.minOf { it.first }, beacons.minOf { it.first })
-        val maxX = max(sensors.maxOf { it.first }, beacons.maxOf { it.first })
+    fun part1(row: Int, input: List<Pair<Sensor, Beacon>>): Int {
+        val sensors = input.map { it.first }
+        val beacons = input.map { it.second }.distinct()
+        val minX = min(sensors.minOf { it.coordinate.first }, beacons.minOf { it.coordinate.first })
+        val maxX = max(sensors.maxOf { it.coordinate.first }, beacons.maxOf { it.coordinate.first })
 
-        val sensorsRadi = sensors.map { sensor ->
-            sensor to beacons.minOf { beacon -> sensor.discreteDistanceTo(beacon) }
+        val sensorsRadi = input.map { (sensor, beacon) ->
+            sensor to sensor.coordinate.discreteDistanceTo(beacon.coordinate)
         }
 
         val maxRadius = sensorsRadi.maxOf { it.second }
 
         return ((minX - maxRadius)..(maxX + maxRadius)).map { x -> x to row }
             .count { currentPos ->
-                if (beacons.contains(currentPos)) false
+                if (beacons.contains(Beacon(currentPos))) false
                 else sensorsRadi.any { (sensor, radiusToBeacon) ->
-                    val distance = currentPos.discreteDistanceTo(sensor)
+                    val distance = currentPos.discreteDistanceTo(sensor.coordinate)
                     distance <= radiusToBeacon
                 }
             }
     }
 
     val testInput = readInput("Day15_test").prepareInput()
-    check(part1(10, testInput.first, testInput.second) == 26)
+    check(part1(10, testInput) == 26)
 
     val input = readInput("Day15").prepareInput()
     measureAndPrintTimeMillis {
-        checkAndPrint(part1(2000000, input.first, input.second), 5100463)
+        checkAndPrint(part1(2000000, input), 5100463)
     }
 }
+
+@JvmInline
+private value class Sensor(val coordinate: Pair<Int, Int>)
+@JvmInline
+private value class Beacon(val coordinate: Pair<Int, Int>)
